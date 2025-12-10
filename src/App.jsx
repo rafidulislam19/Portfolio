443317e1963c53692c86a9b71ff0bf1731501fc2
rafidulislam19 <68073987+rafidulislam19@users.noreply.github.com>
2025-12-10 17:57:58 +0600

    Skills Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import About from './components/about/About'
+import Skills from './components/skills/Skills'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <main className="main">
         <Home />
         <About />
+        <Skills />
       </main>
     </>
   )
