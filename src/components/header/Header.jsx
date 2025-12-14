5af7c3ac8489b6cddc805aaea0c6df87018595d6
Rafidul Islam <rafidulislam057@gmail.com>
2025-12-14 23:49:26 +0600

    Testimonials Component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
                         </a>
                     </li>
                     <li className="nav__item">
-                        <a href="services" className="nav__link">
+                        <a href="#services" className="nav__link">
                             <i className="uil uil-briefcase-alt nav__icon"></i> Services
                         </a>
                     </li>
