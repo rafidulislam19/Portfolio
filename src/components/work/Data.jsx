5d20fa58dde8fcc90aa5fa27a17238e6f0c85755
Rafidul Islam <rafidulislam057@gmail.com>
2026-03-25 00:11:07 +0600

    new project added

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -4,6 +4,7 @@ import Work3 from "../../assets/work3.png";
 import Work4 from "../../assets/work4.png";
 import Work5 from "../../assets/work5.png";
 import Work6 from "../../assets/work6.png";
+import Work7 from "../../assets/work7.png";
 
 export const projectsData = [
   {
@@ -12,7 +13,8 @@ export const projectsData = [
     title: "Learning Management System",
     category: "nextJS",
     tags: ["Next JS", "Tailwind CSS"],
-    description: "This project is a high-performance Learning Management System (LMS) built with Next.js and styled with Tailwind CSS, offering a seamless, responsive experience for delivering and managing online courses.",
+    description:
+      "This project is a high-performance Learning Management System (LMS) built with Next.js and styled with Tailwind CSS, offering a seamless, responsive experience for delivering and managing online courses.",
     demoUrl: "https://learning-management-system-one.vercel.app/",
     githubUrl: "https://github.com/rafidulislam19/Learning-Management-System",
   },
@@ -22,47 +24,63 @@ export const projectsData = [
     title: "Rban Timing Solution",
     category: "reactJS",
     tags: ["React JS", "Material UI"],
-    description: "This project is a real-time race timing and event management solution built with React.js and Material UI (MUI), designed to provide high-precision chip-timing integration, live leaderboard updates, and responsive dashboards for marathon organizers and participants.",
+    description:
+      "This project is a real-time race timing and event management solution built with React.js and Material UI (MUI), designed to provide high-precision chip-timing integration, live leaderboard updates, and responsive dashboards for marathon organizers and participants.",
     demoUrl: "https://learning-management-system-one.vercel.app/",
     githubUrl: "https://github.com/RISHAD-prog/RBAN-Timing-Solution",
   },
   {
     id: 3,
+    image: Work7,
+    title: "Labor Cost Management",
+    category: "nextJS",
+    tags: ["Next JS", "Tailwind CSS"],
+    description:
+      "Labor cost management platform that helps organizations track employee hours, manage schedules, handle contracts, and analyze labor costs with role-based access for administrators, managers, and employees.",
+    demoUrl: "https://planivue-next.vercel.app//",
+    githubUrl: "https://github.com/rafidulislam19/planivue-next",
+  },
+  {
+    id: 4,
     image: Work3,
     title: "Restaurant Management",
     category: "django",
     tags: ["React JS", "Django"],
-    description: "This is a restaurant project which is built using React and Django where customers can choose ingredients accordingly and place orders.",
+    description:
+      "This is a restaurant project which is built using React and Django where customers can choose ingredients accordingly and place orders.",
     demoUrl: "https://myrestaurantreact.netlify.app/home",
     githubUrl: "https://github.com/rafidulislam19/Learning-Management-System",
   },
   {
-    id: 4,
+    id: 5,
     image: Work4,
     title: "Instagram Clone",
     category: "Django",
     tags: ["Python", "Django"],
-    description: "Instagram Clone using Django. Users can follow each other, view followings post and like them, add new post, add new story and story will expire after 1 day, users can comment on followings posts only if they follow each other.",
+    description:
+      "Instagram Clone using Django. Users can follow each other, view followings post and like them, add new post, add new story and story will expire after 1 day, users can comment on followings posts only if they follow each other.",
     demoUrl: "https://safinstaclone.pythonanywhere.com/",
     githubUrl: "https://github.com/rafidulislam19/Learning-Management-System",
   },
   {
-    id: 5,
+    id: 6,
     image: Work5,
     title: "LinkedIn Clone",
     category: "reactJS",
     tags: ["React", "Django"],
-    description: "A small Linkedin Clone where users can connect with each other and send connection request and companies can post job and post about anything. Users can also chat with each other if they are connected.",
+    description:
+      "A small Linkedin Clone where users can connect with each other and send connection request and companies can post job and post about anything. Users can also chat with each other if they are connected.",
     demoUrl: "https://linksafclone.netlify.app/home",
     githubUrl: "https://github.com/rafidulislam19/Learning-Management-System",
   },
-    {
-    id: 6,
+  {
+    id: 7,
     image: Work6,
     title: "Python Expense Tracker Tkinter",
     category: "python",
     tags: ["Python"],
-    description: "This project is a user-friendly, desktop-based expense tracker built using Python and Tkinter, providing intuitive features for logging income and expenses, visualizing spending habits, and managing personal finances locally.",
+    description:
+      "This project is a user-friendly, desktop-based expense tracker built using Python and Tkinter, providing intuitive features for logging income and expenses, visualizing spending habits, and managing personal finances locally.",
     demoUrl: "https://linksafclone.netlify.app/home",
     githubUrl: "https://github.com/rafidulislam19/Learning-Management-System",
   },
